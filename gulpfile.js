f4ba4f5da39a261ed6a8597c5a8e117c45e27f18
Rodolphe <rodolphe@alsacreations.fr>
2015-07-17 13:21:59 +0200

    Ajout de chemins en variables, imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
+// Config
+var path_less_src = './src/assets/css/*.less';
+var path_css_dest = './dist/assets/css/';
+
+var path_js_src = './src/assets/js/src/*.js';
+var path_js_dest = './dist/assets/js/';
+var path_js_dest_filename = 'global.min.js';
+
+var path_svgfolders_src = ['./src/assets/img/svg/'];
+var path_jpgpngfolders_src = ['./src/assets/img/'];
+var path_svgfolders_dest = ['./dist/assets/img/svg/'];
+var path_jpgpngfolders_dest = ['./dist/assets/img/'];
+
 // Requires
 var gulp = require('gulp');
 
@@ -10,31 +23,49 @@ var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
+var merge = require('merge-stream');
 // var styledown = require('gulp-styledown');
 
 // Common tasks
 gulp.task('styles', ['styles-less']);
-gulp.task('doallthethings', ['styles','scripts']);
+gulp.task('doallthethings', ['styles','scripts','images']);
 
 // Styles LESS
 gulp.task('styles-less', function () {
-  return gulp.src('./src/assets/css/*.less')
+  return gulp.src(path_less)
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(rename({suffix: '.min'}))
     //.pipe(sourcemaps.init())
     .pipe(minify())
     //.pipe(sourcemaps.write('.', {includeContent: false}))
-    .pipe(gulp.dest('./dist/assets/css/'));
+    .pipe(gulp.dest(path_css_dest));
 });
-
 
 // Scripts
 gulp.task('scripts',function() {
-  return gulp.src('./src/assets/js/src/*.js')
+  return gulp.src(path_js)
     .pipe(uglify())
-    .pipe(concat('global.min.js'))
-    .pipe(gulp.dest('./dist/assets/js/'));
+    .pipe(concat(path_js_dest_filename))
+    .pipe(gulp.dest(path_js_dest));
+});
+
+// Images optimisées
+gulp.task('images',function() {
+  var tasks_svg = path_svgfolders_src.map(function(folder) {
+    return gulp.src(folder+'*.svg')
+      .pipe(imagemin({
+        svgoPlugins: [{removeViewBox: false}, {cleanupIDs:false}],
+      }))
+      .pipe(gulp.dest(path_svgfolders_dest));
+  });
+  var tasks_jpgpng = path_jpgpngfolders_src.map(function(folder) {
+    return gulp.src(folder+'*.{png,jpg}')
+      .pipe(imagemin())
+      .pipe(gulp.dest(path_jpgpngfolders_dest));
+  });
+  return merge(tasks_svg,tasks_jpgpng);
 });
 
 /*
@@ -51,11 +82,11 @@ gulp.task('styleguide', function() {
 
 // Watcher
 gulp.task('watch', function() {
-  browserSync({
+  /*browserSync({
     proxy: 'http://localhost:5000'
-  });
-  gulp.watch(['./src/assets/css/*.less'], ['styles-less', browserSync.reload /*,'styleguide'*/ ]);
-  gulp.watch(['./src/assets/js/src/*.js'], ['scripts']);
+  });*/
+  gulp.watch([path_less], ['styles-less', /*browserSync.reload, */ /*'styleguide'*/ ]);
+  gulp.watch([path_js], ['scripts']);
 });
 
 gulp.task('default', ['doallthethings']);
